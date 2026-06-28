3b8dd2ac74ab59bc9b858b79816e0aa1757529b3
piyalp931-del <piyalp931@gmail.com>
2026-06-28 11:52:04 +0530

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -2,7 +2,6 @@
 // firebase/firebase-config.js - Firebase Initialization
 // ================================================================
 
-// ඔබේ සැබෑ Firebase Config එක (ඔබ දුන්නු එක)
 const firebaseConfig = {
   apiKey: "AIzaSyBopvCfBcrP-RNvGiho53qfAlbtQqepn4Q",
   authDomain: "jdms-prod.firebaseapp.com",
@@ -13,15 +12,12 @@ const firebaseConfig = {
   measurementId: "G-Y6LY2V1G9H"
 };
 
-// Firebase Initialize කරන්න (Compat SDK සඳහා)
 firebase.initializeApp(firebaseConfig);
 
-// පහසුව සඳහා Global References හදාගන්න
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-// Firestore Offline Persistence Enable කරන්න (අන්තර්ජාලය නැති වෙලාවට)
 db.enablePersistence()
   .then(() => console.log('🔥 Offline persistence enabled'))
   .catch(err => console.warn('Firestore persistence error:', err));
